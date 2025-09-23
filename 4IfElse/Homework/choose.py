83f30b40a3af2ae218d646d171231a82973815b7
jooshhart <jooshhart@gmail.com>
2025-09-23 11:15:29 -0700

    one little change was made for the homework instructions

--- 4IfElse/Homework/choose.py
+++ 4IfElse/Homework/choose.py
@@ -2,6 +2,7 @@
 Create a choose adventure game.
 Give three options for the first input.
 Each option should lead to another input with three options.
+The third input should lead to an outcome.
 Use if, elif, and else statements to handle the choices.
 Use print statements to describe the outcomes of each choice.
 Use comments to explain your code.
